ce2b63b6fa987b707d390e809ea007832f1db79b
Rendisaputra33 <gorengmbah@gmail.com>
2021-09-09 19:20:20 +0700

    update

--- application/models/MRincian.php
+++ application/models/MRincian.php
@@ -4,36 +4,36 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class MRincian extends CI_Model
 {
 
-    public function getData()
-    {
-        $now = date('Y-m-d');
-        $data = $this->db->query("SELECT
-    	rincian.id_rincian as id_rincian,
-        rincian.tanggal_rincian as tanggal_rincian,
-        rincian.debit_rincian AS debit_rincian,
-        kode1.nama_kode AS nama_debit,
-        rincian.keterangan_rincian AS keterangan_rincian,
-        rincian.nominal_rincian AS nominal_rincian,
-        rincian.kredit_rincian AS kredit_rincian,
-        kode2.nama_kode AS nama_kredit
-    FROM
-        tb_rincian AS rincian
-    JOIN tb_kode AS kode1
-    ON
-        rincian.debit_rincian = kode1.kode_akun AND rincian.debit_rincian = kode1.kode_akun
-    JOIN tb_kode AS kode2
-    ON
-        rincian.kredit_rincian = kode2.kode_akun AND rincian.kredit_rincian = kode2.kode_akun 
-        WHERE tanggal_rincian >= '$now'
-        ORDER BY id_rincian ASC")->result_array();
-        return $data;
-    }
-
     // public function getData()
     // {
-    //     $data = $this->db->query('SELECT * FROM tb_rincian JOIN tb_kode ON tb_rincian.debit_rincian = tb_kode.kode_akun')->result_array();
+    //     $now = date('Y-m-d');
+    //     $data = $this->db->query("SELECT
+    // 	rincian.id_rincian as id_rincian,
+    //     rincian.tanggal_rincian as tanggal_rincian,
+    //     rincian.debit_rincian AS debit_rincian,
+    //     kode1.nama_kode AS nama_debit,
+    //     rincian.keterangan_rincian AS keterangan_rincian,
+    //     rincian.nominal_rincian AS nominal_rincian,
+    //     rincian.kredit_rincian AS kredit_rincian,
+    //     kode2.nama_kode AS nama_kredit
+    // FROM
+    //     tb_rincian AS rincian
+    // JOIN tb_kode AS kode1
+    // ON
+    //     rincian.debit_rincian = kode1.kode_akun AND rincian.debit_rincian = kode1.kode_akun
+    // JOIN tb_kode AS kode2
+    // ON
+    //     rincian.kredit_rincian = kode2.kode_akun AND rincian.kredit_rincian = kode2.kode_akun 
+    //     WHERE tanggal_rincian >= '$now'
+    //     ORDER BY id_rincian ASC")->result_array();
     //     return $data;
     // }
+
+    public function getData()
+    {
+        $data = $this->db->query('SELECT * FROM tb_rincian JOIN tb_kode ON tb_rincian.debit_rincian = tb_kode.kode_akun')->result_array();
+        return $data;
+    }
 
     public function addPengeluaran($data_array)
     {
